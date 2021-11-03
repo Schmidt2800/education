c9e96f579a5f9535f32111297aa1569a78ae1401
rsc <rsc@unik.dk>
2021-11-03 10:12:02 +0100

    filewrite

---
+++ MicroServiceEduDocumentGenerator/MicroServiceEduDocumentGenerator/Domain/Model/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroServiceEduDocumentGenerator.Domain.Model
+{
+    public class Order
+    {
+        public string OrderNo { get; set; }
+        public int CustomerNo { get; set; }
+        public string CustomerName { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
